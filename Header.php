af94ec47e3e5747440fe08de0fa1cbd680a9a754
diegomaes01 <119438182+diegomaes01@users.noreply.github.com>
2023-11-17 12:08:20 +0100

    Add files via upload

--- Header.php
+++ Header.php
@@ -22,13 +22,38 @@
     <link rel="alternate stylesheet" href="PI(LetraGrande).css" title="modo de letra grande">
     <link rel="alternate stylesheet" href="PI(LetraYContraste).css" title="modo de letra grande y alto contraste">
     <link rel="stylesheet" href="PI(impresión).css" media="print" >
-    
-    
-    
-    
+  
 </head>
 <body>
-<header> <!--Boton que te lleve al Formulario de acceso-->
+<header> 
+    <?php
+    if(isset($_COOKIE["usuario"]) || isset($_SESSION["estilos"])){//si tienes usuario registrado te sale el header del Usuario
+        echo<<<hereDoc
+        <div class="cabezeraizq">
+            <img src="Diseño_sin_título-removebg-preview.png" alt="logotipo" class="img">
+            <p class="site-title">PI</p>
+        </div>
+        <div class="cabezerader">
+            <!--Boton para el formulario de busqueda-->
+            <nav id="menu-ppal">
+                <ul>
+                <!--Boton para el formulario de busqueda-->
+                <li><a href="Formulario_de_imagen_logueado.php"> Busqueda detallada</a></li>
+                </ul>
+            </nav>
+           
+            
+            <form action="resultado_de_busqueda_logueado.php"  class="buscador-form">
+                <p><input type="text" name="buscador" id="buscador">
+            </form>
+            <a href="resultado_de_busqueda_logueado.php"> <span class="material-symbols-outlined">search </span></a>
+            <a href="Mi_perfil.php"><p><img src="perfil-mujer-vivo.png" alt="logotipo" class="foto_perfil"></p></a>
+        </div>  
+        hereDoc;
+
+    }
+    else{
+        echo<<<hereDoc
         <div class="cabezeraizq">
             <img src="Diseño_sin_título-removebg-preview.png" alt="logotipo" class="img">
             <p class="site-title">PI</p>
@@ -52,6 +77,12 @@
                 </form>
                 <a href="resultado_de_busqueda.php"> <span class="material-symbols-outlined">search </span></a>
         </div>
+        hereDoc;
+    }
+    
+    ?>
+<!--Boton que te lleve al Formulario de acceso-->
+        
         
     </header>
 </body>
