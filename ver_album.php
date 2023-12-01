819cdcdee358b27c7a53d64d916464ff33a11044
diegomaes01 <119438182+diegomaes01@users.noreply.github.com>
2023-12-01 09:33:13 +0100

    Add files via upload

--- ver_album.php
+++ ver_album.php
@@ -6,23 +6,18 @@ include "Header.php";
 if(!isset($_COOKIE["usuario"]) && !isset($_SESSION["nombre"])){//si no esta registrado
     header('Location: error_de_login.html');
 }
-
-
-
 $id_Album = isset($_GET["id"]) ? $_GET["id"] : null;
 $df = isset($_GET["df"]) ? $_GET["df"] : null;
-
-if($df==1){
-echo"hola loco";
-
-}
 $result = mysqli_query($id, "SELECT * FROM fotos f
 JOIN albumes a ON f.Album = a.IdAlbum
 JOIN usuarios u ON a.Usuario = u.IdUsuario
-JOIN paises p ON  f.Pais= p.Idpais WHERE a.IdAlbum=$id_Album");
-    
-    $cantidadRegistros = mysqli_num_rows($result);
+JOIN paises p ON  f.Pais= p.Idpais WHERE a.IdAlbum=$id_Album"); 
+$cantidadRegistros = mysqli_num_rows($result);
 
+$result2=mysqli_query($id,"SELECT MIN(Fecha) AS fechaAntigua, MAX(Fecha) AS fechaReciente FROM fotos WHERE Album=$id_Album");
+$intervalo= mysqli_fetch_assoc($result2);
+$fechaAntigua=$intervalo['fechaAntigua'];
+$fechaReciente=$intervalo['fechaReciente'];
 
 ?>
 <body>
@@ -34,6 +29,8 @@ JOIN paises p ON  f.Pais= p.Idpais WHERE a.IdAlbum=$id_Album");
                 $row = mysqli_fetch_array($result);
                 echo"<p>Este album contiene {$cantidadRegistros} imagenes.</p>";
                 echo"<p>Descripcion: {$row['Descripcion']} </p>";
+                echo"<p>La primera foto fue subida el: $fechaAntigua </p>";
+                echo"<p>La ultima foto fue subida el: $fechaReciente </p>";
             ?>
         </div>
     </div>
@@ -59,6 +56,12 @@ JOIN paises p ON  f.Pais= p.Idpais WHERE a.IdAlbum=$id_Album");
 
             hereDoc;
 
+        }
+
+
+        if($df==1){
+            echo"<a href='Añadir_Foto.php?id={$_SESSION["idUser"]}' class='navegadores'>Añadir foto a album</a>";
+            
         }
     ?>
     </div>
